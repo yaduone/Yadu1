1db129b392321ae9a24ac9c19fdbcd6610327ad1
lal5hivam <l.singhshivam1@gmail.com>
2026-04-25 03:13:34 +0530

    Admin Panel Deployment

--- admin-panel/vite.config.js
+++ admin-panel/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://yadu1.up.railway.app',
         changeOrigin: true,
       },
     },
